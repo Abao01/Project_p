7227fd895656c76383a0cf1cca020400f3b617a8
hxueting <77209297+Abao01@users.noreply.github.com>
2022-04-05 12:56:40 -0500

    Update db_operations.py

--- db_operations.py
+++ db_operations.py
@@ -1,9 +1,16 @@
+"""This model contains a dboperations class with functions to
+initialize and create a database, fetch data and purge data."""
 import sqlite3
+
+from numpy import insert
 from dbcm import DBCM
 from scrape_weather import WeatherScraper
 
 class DBOperations:
+  """This class contains functions to
+  initialize and save data, fetch data and purge data."""
   def initialize_db(self):
+    """creates the table."""
     with DBCM("weather.sqlite") as cur:
       try:
         cur.execute("""create table if not exists weatherdata
@@ -18,25 +25,34 @@ class DBOperations:
             print("Error creating table:", e)
 
   def save_date(self,dictionary):
+    """insert all the data from the DB"""
+    try:
+      sql = """insert into weatherdata (sample_date,location,max_temp,min_temp,avg_temp)
+              values (?,?,?,?,?)"""
       try:
-        sql = """insert into weatherdata (sample_date,location,max_temp,min_temp,avg_temp)
-                values (?,?,?,?,?)"""
-        try:
-          for dates,datas in dictionary.items():
-            data = []
-            data.append(dates)
-            data.append("Winnipeg, MB")
-            for key, value in datas.items():
-                data.append(value)
-            with DBCM("weather.sqlite") as cur:
-              cur.execute(sql, data)
-          print("Added weatherdata successfully.")
-        except Exception as e:
-          print("Error: ",e)
+        for dates,datas in dictionary.items():
+          data = []
+          data.append(dates)
+          data.append("Winnipeg, MB")
+          for key, value in datas.items():
+              data.append(value)
+          with DBCM("weather.sqlite") as cur:
+            cur.execute(sql, data)
+        print("Added weatherdata successfully.")
       except Exception as e:
-        print("Error inserting weatherdata.", e)
+        print("Error: ",e)
+    except Exception as e:
+      print("Error inserting weatherdata.", e)
+
+  def fetch_date(self):
+    """fetch all the data from the DB"""
+    with DBCM("weather.sqlite") as cur:
+      sql = """select * from weatherdata"""
+      for row in cur.execute(sql):
+        print(row)
 
   def purge_data(self):
+    """purge all the data from the DB"""
     with DBCM("weather.sqlite") as cur:
       sql= """delete from  weatherdata"""
       for row in cur.execute(sql):
@@ -50,5 +66,5 @@ weather.fetch_weather_data()
 db = DBOperations()
 db.initialize_db()
 db.save_date(weather.weather)
-
-
+db.fetch_date()
+# db.purge_data()
